caafb879125e2ee18a46fa8b00e33d39ea089e64
AndrewMTitus <112717415+AndrewMTitus@users.noreply.github.com>
2023-03-20 23:24:05 -0400

    update the code.html

--- js/site.js
+++ js/site.js
@@ -1,15 +1,10 @@
 //Get the string from the page
 //controller function
 function getValue(){
-
     document.getElementById("alert").classList.add("invisible");
-
     let userString = document.getElementById("userString").value;
-
     let revString = reverseString(userString);
-
     displayString(revString);
-
 }
 
 //Reverse the string
@@ -17,25 +12,16 @@ function getValue(){
 function reverseString(userString){
 
     let revString = [];
-
-    //Reference for how strings and arrays work
-    //let name = Andrew
-    //name[0] = "A";
-    //name[5] = "w";
-    //last position in an array name.length -1
-
     //reverse a string using a for loop
     for (let index = userString.length -1 ; index >= 0; index--) {
         revString += userString[index];
     }
-
     return revString;
 }
 
 //Display the reversed string to the user
 //View Function
 function displayString(revString){
-
     //write to the page
     document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
     //show the alert box
